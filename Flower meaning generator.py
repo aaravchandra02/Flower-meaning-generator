e92e0a70c0d771779a2afd9c69dc42b9cb680433
Aarav Chandra <aarav@Aaravs-MBP.home>
2020-07-08 10:13:48 -0700

    Initial commit

---
+++ Flower meaning generator.py
@@ -0,0 +1,39 @@
+from linked_list import Node, LinkedList
+from list_blossom import flower_definitions
+
+
+class HashMap:
+
+    def __init__(self, size):
+        self.array_size = size
+        self.array = [LinkedList() for i in range(size)]
+
+    def hash(self, key):
+        return sum(key.encode())
+
+    def compress(self, hash_code):
+        return hash_code % self.array_size
+
+    def assign(self, key, value):
+        array_index = self.compress(self.hash(key))
+        payload = Node([key, value])
+        list_at_array = self.array[array_index]
+        for i in list_at_array:
+            # if key is same as the one present in the array at that position
+            if i[0] == key:
+                i[1] = value
+        list_at_array.insert(payload)
+
+    def retrieve(self, key):
+        array_index = self.compress(self.hash(key))
+        list_at_list = self.array[array_index]
+        for i in list_at_list:
+            if i[0] == key:
+                return i[1]
+        return None
+
+
+blossom = HashMap(len(flower_definitions))
+for item in flower_definitions:
+    blossom.assign(item[0], item[1])
+print(blossom.retrieve('daisy'))
